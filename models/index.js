142cddf1958a24e53df56df78ca45f443e4f8a5a
Frank Fernandez <frankfernandez@Franks-Macbook.local>
2019-07-27 17:36:47 +0900

    new files

---
+++ models/index.js
@@ -0,0 +1,28 @@
+const moment = require("moment");
+
+const Zodiac = function(dbZodiac) {
+  this.name = dbZodiac.name;
+  this.lead_month = dbZodiac.lead_month;
+  this.symbol = dbZodiac.symbol;
+  this.trait = dbZodiac.trait;
+  this.translation = dbZodiac.translation;
+  this.longitude = dbZodiac.longitude;
+};
+
+Zodiaca = function() {
+  return {
+   name :this.name,
+    leadMonth : this.lead_month,
+    symbol:this.symbol ,
+    trait:this.trait,
+    translation:this.translation,
+    longitude:this.longitude
+  };
+};
+
+module.exports = (knex) => {
+  return {
+    create: require("./create")(knex, Zodiac),
+    list: require("./list")(knex, Zodiac),
+  };
+};
