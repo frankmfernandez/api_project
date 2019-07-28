952b75ac4d60fc622e6a02ffcb302af13c75988e
Frank Fernandez <frankfernandez@Franks-Macbook.local>
2019-07-29 07:18:38 +0900

    edits

---
+++ README.md
@@ -0,0 +1,11 @@
+API PROJECT
+Background
+We were given a weekend to create our own api. I decided to work with express to create the server and utilized knex to connect javascript to the database. The contents of the database include one table that contains basic zodiac data.
+Set Up
+This is a personal preference, but found it extremely useful downloading insomnia and using it easily check queries made to the database. You will want to run "yarn start" to set up the node and "yarn dev" to set nodemon to keep a pulse on the server.
+"https://insomnia.rest/download/#mac" Download Insomnia
+Functionality
+List queries "/zodiacs", Get queries "/zodiacs/:name", Create queries "/zodiacs" + pass through JSON {"name":newName}, Delete queries "zodiacs/:name", Post queries "zodiacs/:name" + JSON{"name":newName}
+Please click the following to access this project via github
+
+"https://github.com/frankmfernandez/api_project">Git Hub Link
--- index.js
+++ index.js
@@ -3,13 +3,20 @@ const services = require("./services")(config);
 const knex = require("knex")(config.db);
 const models = require("./models")(knex);
 const apiRouter = require("./controllers")(models);
-const bodyParser = require("body-parser"); // a middleware plugin to enable express to parse JSON
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 app.use(bodyParser.json({ type: "application/json", limit: "50mb" }));
 app.use("/api", apiRouter);
-app.use(express.static(`${__dirname}/views`)); // otherwise load the client app
+const path = require("path");
+app.use(express.static(`${__dirname}/views`));
 
+app.use(express.static("views"));
+app.get("/", function(req, res) {
+  res.sendFile("/index.html");
+});
+
+//app.use()
 app.use((err, req, res, next) => {
   if (err.stack) {
     if (err.stack.match("node_modules/body-parser"))
@@ -23,39 +30,3 @@ app.use((err, req, res, next) => {
 app.listen(config.express.port, () => {
   services.logger.log(`Server up and listening on port ${config.express.port}`);
 });
-
-//const config = require("./config");
-
-// const services = require("./services")(config);
-
-// const knex = require("knex")(config.db);
-
-//const morgan = require("morgan"); // a popular library for logging your requests
-
-// const express = require("express");
-
-// const app = express();
-
-// 1. log every request when it comes in
-// app.use(morgan("dev"));
-
-// 2. Set the headers for incoming requests
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Methods",
-//     "GET,PUT,POST,DELETE,OPTIONS,PATCH"
-//   );
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, authorization"
-//   );
-//   next();
-// });
-
-// 3. Parse request bodies as json
-
-// 4. If the requests begin with '/api', hand them off to the API router
-
-// 5. Catch unhandled errors thrown by any of the previous middleware steps
-// eslint-disable-next-line no-unused-vars
